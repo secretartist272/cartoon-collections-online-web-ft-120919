5a8642237120e2c1f3bc14dcf93d0693df65b694
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-18 00:13:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,5 +21,8 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   i = 0
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+   while i < cheese_types.length
+    return cheese_types[i] if array.include?(cheese_types[i])
+    i += 1
+  end
 end
